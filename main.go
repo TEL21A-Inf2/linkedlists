ec5e5ca6224e050f21d29207d4acb4e23536d0a0
Reiner Hüchting <dev@reinerhuechting.de>
2022-05-07 12:04:15 +0200

    Das Beispiel in der main()-Funktion verwendet jetzt den Listen-Datentyp statt des Element-Typs.

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/tel21a-inf2/linkedlists/sllist"
 )
 
 func main() {
-	l1 := sllist.NewSinglyLinkedListElementInt()
-	l2 := sllist.NewSinglyLinkedListElementInt()
-	l3 := sllist.NewSinglyLinkedListElementInt()
+	l1 := sllist.NewSinglyLinkedListInt()
 
-	l1.SetNext(l2)
-	l2.SetNext(l3)
+	l1.Append(42)
+	l1.Append(25)
+	l1.Append(77)
+	l1.Append(38)
+
+	fmt.Println(l1)
 }
