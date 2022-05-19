881f108fe1c2a58acb93141b4af5afdfb0b795db
Reiner Hüchting <dev@reinerhuechting.de>
2022-05-19 20:35:55 +0200

    Lösung für Swap() noch um den Fall ergänzt, dass die beiden Elemente direkt nebeneinander liegen.

--- sllist/aufgabeswap.go
+++ sllist/aufgabeswap.go
@@ -19,10 +19,36 @@ Passen Sie auch die Tests an bzw. erweitern Sie diese.
 */
 
 func (list *SinglyLinkedListInt) Swap(pos1, pos2 int) error {
+
+	// "Organisatorisches": Sonderfälle und umgedrehte Listenpositionen abfangen.
+	if pos1 == pos2 {
+		return nil
+	}
 	length := list.Length()
 	if pos1 < 0 || pos1 >= length || pos2 < 0 || pos2 >= length {
 		return errors.New("error: swap positions out of range")
 	}
+	if pos1 > pos2 {
+		return list.Swap(pos2, pos1)
+	}
+
+	// Was übrig bleibt, sind zwei Fälle:
+	// 1. pos1 und pos2 liegen direkt nebeneinander.
+	// 2. pos1 und pos2 liegen nicht direkt nebeneinander.
+	// Für diese beiden Fälle gibt es jeweils eine Spezialfunktion,
+	// die wir hier aufrufen.
+	if pos1 == pos2-1 {
+		list.swapNext(pos1)
+		return nil
+	}
+	list.swapNonAdjacent(pos1, pos2)
+	return nil
+}
+
+// Intern benutzte Hilfsmethode, um zwei beliebige nicht benachbarte Elemente
+// zu vertauschen. Es gibt keine Sicherheitsprüfungen und keine Fehlerbehandlung.
+// Die Methode darf nur aufgerufen werden, wenn sicher ist, dass sie auch funktioniert.
+func (list *SinglyLinkedListInt) swapNonAdjacent(pos1, pos2 int) {
 
 	// Wir holen uns die Elemente an den Stellen pos1 und pos2
 	// und nennen sie B und E:
@@ -55,5 +81,33 @@ func (list *SinglyLinkedListInt) Swap(pos1, pos2 int) error {
 	} else {
 		list.head = E
 	}
-	return nil
+}
+
+// Intern benutzte Hilfsmethode, um ein Element mit seinem Nachfolger
+// zu vertauschen. Es gibt keine Sicherheitsprüfungen und keine Fehlerbehandlung.
+// Die Methode darf nur aufgerufen werden, wenn sicher ist, dass sie auch funktioniert.
+func (list *SinglyLinkedListInt) swapNext(pos1 int) {
+
+	// Wir holen uns die beiden Elemente und ihre Vorgänger und Nachfolger.
+	A := list.head.GetElement(pos1 - 1)
+	B := list.head.GetElement(pos1)
+	C := list.head.GetElement(pos1 + 1)
+	D := list.head.GetElement(pos1 + 2)
+
+	/* Wir haben jetzt folgende Listenstruktur:
+
+	... -> A -> B -> C -> D -> ...
+
+	B und C sollen getauscht werden.
+	Wir verbiegen die Pointer.
+	*/
+	B.next = D
+	C.next = B
+
+	// Für A gibt es wieder den Sonderfall, falls B der Kopf der Liste ist.
+	if A != nil {
+		A.next = C
+	} else {
+		list.head = C
+	}
 }
--- sllist/aufgabeswap_test.go
+++ sllist/aufgabeswap_test.go
@@ -27,6 +27,9 @@ func ExampleSinglyLinkedListElementInt_Swap() {
 	err3 := l1.Swap(-1, 10)
 	fmt.Println(err3)
 
+	l1.Swap(2, 3)
+	fmt.Println(l1)
+
 	// Output:
 	// [ 42 25 77 50 103 38 ]
 	// [ 42 25 103 50 77 38 ]
@@ -34,4 +37,5 @@ func ExampleSinglyLinkedListElementInt_Swap() {
 	// error: swap positions out of range
 	// error: swap positions out of range
 	// error: swap positions out of range
+	// [ 77 25 50 103 42 38 ]
 }
